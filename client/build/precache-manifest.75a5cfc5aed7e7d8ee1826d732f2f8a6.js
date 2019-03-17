b792620556ae84efef3ce3572e725ce3789bcdd8
Daniel Duffy <dpd1208@gmail.com>
2019-03-17 15:53:44 -0600

    build

---
+++ client/build/precache-manifest.75a5cfc5aed7e7d8ee1826d732f2f8a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dc9ddd5c48234824ef55",
+    "url": "/static/js/main.9f87456f.chunk.js"
+  },
+  {
+    "revision": "f1711d323b08b05a6695",
+    "url": "/static/js/2.a6d27205.chunk.js"
+  },
+  {
+    "revision": "dc9ddd5c48234824ef55",
+    "url": "/static/css/main.22ffbbc6.chunk.css"
+  },
+  {
+    "revision": "6ecf70614e43c41ef9be7ec3530a2f50",
+    "url": "/index.html"
+  }
+];
